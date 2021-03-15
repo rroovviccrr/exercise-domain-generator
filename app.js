480ef63ce205d5cf19942acac67b085c0634baf5
_rovi.cr <rovicr@live.com>
2021-03-15 03:29:07 +0000

    ramdongenerator

---
+++ app.js
@@ -0,0 +1,16 @@
+
+let adj = ['the', 'our' ];
+let noun = ['great', 'big'];
+let domain = ['.com', '.org' , '.net'];
+
+adj.forEach(item => {
+    domain.forEach(domain =>{
+        console.log( item + noun + domain);
+        
+    })
+        
+});
+
+
+
+
